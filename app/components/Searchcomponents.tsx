5a6e5420cce12b5412ca62032cbfa673b9888367
Regino <126629416+Rejeno@users.noreply.github.com>
2024-05-20 22:44:21 +0800

    new commit

--- app/components/Searchcomponents.tsx
+++ app/components/Searchcomponents.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { Search } from "lucide-react";
+import { FormEvent, useState } from 'react';
 import { useCountries } from "../lib/getCountries";
 import { Counter } from "./Counter";
 import { HomeMap } from "./HomeMap";
@@ -22,23 +19,36 @@ export function SearchModalComponent() {
     const { getAllCountries } = useCountries();
     const [guests, setGuests] = useState(0);
     const [rooms, setRooms] = useState(0);
+    const [error, setError] = useState("");
 
     function handleNextClick() {
         if (step === 1 && !locationValue) {
-            toast.error("Please select a country.");
+            setError("Please select a country.");
             return;
         }
         if (step === 2 && (!guests || !rooms)) {
             if (!guests) {
-                toast.error("Please select the number of guests.");
+                setError("Please select the number of guests.");
                 return; // Halt execution
             }
             if (!rooms) {
-                toast.error("Please select the number of rooms.");
+                setError("Please select the number of rooms.");
                 return; // Halt execution
             }
         }
         setStep(step + 1);
+    }
+
+    function handleFormSubmit(event: FormEvent) {
+        if (step === 2 && (!guests || !rooms)) {
+            event.preventDefault();
+            if (!guests) {
+                setError("Please select the number of guests.");
+            }
+            if (!rooms) {
+                setError("Please select the number of rooms.");
+            }
+        }
     }
 
     function SubmitButtonLocal() {
@@ -48,14 +58,24 @@ export function SearchModalComponent() {
                     Next
                 </Button>
             );
-        } else if (step === 2 ) {
+        } else if (step === 2) {
             return <CreationSubmit />;
         }
     }
 
     return (
         <>
-            <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+            <Dialog open={!!error} onOpenChange={() => setError("")}>
+                <DialogContent className="border border-red-500 border-width:4px">
+                    <DialogHeader>
+                        <DialogTitle className="font-bold text-red-500">Error!</DialogTitle>
+                        <DialogDescription>{error}</DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <Button onClick={() => setError("")} className="bg-red-500 text-white">OK</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
             <Dialog>
                 <DialogTrigger asChild>
                     <div className="flex items-center justify-between rounded-full mr-4 py-1 sm:py-2 px-2 sm:px-4 border hover:shadow-md dark:hover:shadow-white cursor-pointer">
@@ -68,14 +88,12 @@ export function SearchModalComponent() {
                     </div>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-[425px]">
-                    <form className="gap-4 flex flex-col">
+                    <form className="gap-4 flex flex-col" onSubmit={handleFormSubmit}>
                         <input type="hidden" name="country" value={locationValue} />
                         {step === 1 ? (
                             <>
                                 <DialogHeader>
-                                    <DialogTitle>
-                                        Select a Country
-                                    </DialogTitle>
+                                    <DialogTitle>Select a Country</DialogTitle>
                                     <DialogDescription>
                                         Please Select a country, For Us to Know Your Destination
                                     </DialogDescription>
@@ -87,9 +105,7 @@ export function SearchModalComponent() {
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectGroup>
-                                            <SelectLabel>
-                                                Countries
-                                            </SelectLabel>
+                                            <SelectLabel>Countries</SelectLabel>
                                             {getAllCountries().map((item) => (
                                                 <SelectItem key={item.value} value={item.value}>
                                                     {item.flag} {item.label} / {item.region}
@@ -104,50 +120,36 @@ export function SearchModalComponent() {
                         ) : (
                             <>
                                 <DialogHeader>
-                                    <DialogTitle className="-mt-2">
-                                        Please provide all the information
-                                    </DialogTitle>
+                                    <DialogTitle className="-mt-2">Please provide all the information</DialogTitle>
                                 </DialogHeader>
 
                                 <Card>
                                     <CardHeader className="flex flex-col gap-y-2 -mt-2">
                                         <div className="flex items-center justify-between">
                                             <div className="flex flex-col">
-                                                <h3 className="underline font-medium font-semibold">
-                                                    Guests
-                                                </h3>
-                                                <p className="text-muted-foreground text-sm">
-                                                    How many guests?
-                                                </p>
+                                                <h3 className="underline font-medium font-semibold">Guests</h3>
+                                                <p className="text-muted-foreground text-sm">How many guests?</p>
                                             </div>
                                             <Counter name="guests" value={guests} onChange={setGuests} />
                                         </div>
 
                                         <div className="flex items-center justify-between">
                                             <div className="flex flex-col">
-                                                <h3 className="underline font-medium font-semibold">
-                                                    Rooms
-                                                </h3>
-                                                <p className="text-muted-foreground text-sm">
-                                                    How many rooms do you need? 
-                                                </p>
+                                                <h3 className="underline font-medium font-semibold">Rooms</h3>
+                                                <p className="text-muted-foreground text-sm">How many rooms do you need?</p>
                                             </div>
                                             <Counter name="rooms" value={rooms} onChange={setRooms} />
                                         </div>
                                         <div className="flex flex-col">
-                                            <h3 className="underline font-medium font-semibold">
-                                                Date
-                                            </h3>
-                                            <p className="text-muted-foreground text-sm mb-2">
-                                                When are you planning to go?
-                                            </p>
+                                            <h3 className="underline font-medium font-semibold">Date</h3>
+                                            <p className="text-muted-foreground text-sm mb-2">When are you planning to go?</p>
                                             <SelectSearchCalender />
                                         </div>
                                     </CardHeader>
                                 </Card>
                             </>
                         )}
-                        <DialogFooter className='-mt-2 -mb-2'>
+                        <DialogFooter className="-mt-2 -mb-2">
                             <SubmitButtonLocal />
                         </DialogFooter>
                     </form>
